51750e97930261ad6f6dd846152b08ca8c430e2c
Tomap <piartt@gmail.com>
2016-06-10 15:18:21 +0200

    initial commit

---
+++ VSTSClient/Client.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.Services.Client;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+using System.Net;
+
+namespace Middleware.SharedTools
+{
+    static class Client
+    {
+        private const string vso_userName = "toto";
+        private const string vso_url = "https://my.visualstudio.com/DefaultCollection";
+        
+        public static T GetClient<T>(string token) where T : VssHttpClientBase
+        {
+            var vs = new VssCredentials(new WindowsCredential(new NetworkCredential(vso_userName, token)));
+            var vsconn = new VssConnection(new Uri(vso_url), vs);
+            return vsconn.GetClient<T>();
+        }
+    }
+}
---
+++ VSTSClient/ConvertWorkItemId.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Middleware.SharedTools;
+
+namespace VSTSClient
+{
+    public class ConvertWorkItemId
+    {
+        private WorkItemTrackingHttpClient _workItemClient;
+
+        public ConvertWorkItemId(string token)
+        {
+            _workItemClient = Client.GetClient<WorkItemTrackingHttpClient>(token);
+        }
+        
+
+    }
+}
